fd3c97b5b96bc6aacd4b9cc256a0e5702c438a3e
dmtr <mcompote@gmail.com>
2018-09-21 17:18:38 +0700

    - дописан popuppify для сохранения в LocalStorage последних введенных  в элементы формы данных

--- js/script.js
+++ js/script.js
@@ -1,37 +1,18 @@
-
-
-
-
-document.addEventListener('DOMContentLoaded', function(evt) {
-    
-    // для form-filter в catalog.html оживлялка output'ов
-    let inputMinCost  = document.querySelector('#form_filter__min_cost_input'  );
-    let outputMinCost = document.querySelector('#form_filter__min_cost_output' );
-    let inputMaxCost  = document.querySelector('#form_filter__max_cost_input'  );
-    let outputMaxCost = document.querySelector('#form_filter__max_cost_output' );
-
-    if( inputMinCost && inputMaxCost && outputMinCost && outputMaxCost ) {
-        inputMinCost.addEventListener('change', function(evt) {
-            outputMinCost.innerHTML = evt.target.value;
-        });
-    
-        inputMaxCost.addEventListener('change', function(evt) {
-            outputMaxCost.innerHTML = evt.target.value;
-        });
-    }
-
+document.addEventListener('DOMContentLoaded', function(evt) {   
 
     sliderify( document.querySelector ('.services') );
     sliderify( document.querySelector ('.promo'   ) );
+
     popuppify(  document.body, 
-                { animation: {
-                    fadeInClasses: ['animation-fade-in'],
-                    fadeOutClasses: ['animation-fade-out'],
-                    animationDuration: 0.1      
-                  } 
+                { 
+                    animation: {
+                        fadeInClasses: ['animation-fade-in'],
+                        fadeOutClasses: ['animation-fade-out'],
+                        animationDuration: 0.1 
+                        },
+                    memorizeFormInputsValues: true
                 } 
     );            
-
 
 
     //убрать ugly-block, показать pretty-block в фильтре товаров на catalog.html
@@ -79,6 +60,7 @@ function popuppify(rootElement = document.body, options = {}) {
         
         registerOpenPopupHandlers( popupRoot );
         registerClosePopupHandlers( popupRoot );
+        registerPopupFormsHandlers( popupRoot );
     }
 
     /**
@@ -112,6 +94,18 @@ function popuppify(rootElement = document.body, options = {}) {
         });
     }
 
+    function registerEscKeyboardEvent( popupRoot ) {
+        document.addEventListener('keydown', evt => {
+            if ( !popupRoot.classList.contains('none') ) {
+                let keycode = (typeof evt.keyCode !='undefined' && evt.keyCode) ? evt.keyCode : evt.which;
+                if (keycode === 27) {
+                    closePopupAndOverlay( popupRoot, POPUP_OVERLAY );
+                };
+            }
+        });
+    }
+
+
     function closePopupAndOverlay( popupRoot, overlay ) {
         if( popupRoot ) {
             if( options && options.animation && options.animation.fadeOutClasses )
@@ -139,17 +133,6 @@ function popuppify(rootElement = document.body, options = {}) {
             overlay.classList.remove('none');
     }
 
-    function registerEscKeyboardEvent( popupRoot ) {
-        document.addEventListener('keydown', evt => {
-            if ( !popupRoot.classList.contains('none') ) {
-                let keycode = (typeof evt.keyCode !='undefined' && evt.keyCode) ? evt.keyCode : evt.which;
-                if (keycode === 27) {
-                    closePopupAndOverlay( popupRoot, POPUP_OVERLAY );
-                };
-            }
-        });
-    }
-
 
     /**
      * @param {HTMLElement} popupRoot 
@@ -161,6 +144,9 @@ function popuppify(rootElement = document.body, options = {}) {
         registerEscKeyboardEvent( popupRoot );
     }
 
+    /**
+     * @param {HTMLElement} popupRoot 
+    */
     function registerOpenPopupHandlers( popupRoot ) {
         let dataAttr = popupRoot.dataset.jsPopuppifyName;
         if( dataAttr )
@@ -169,9 +155,263 @@ function popuppify(rootElement = document.body, options = {}) {
                  .forEach( link => registerLinkHandler(link, popupRoot) );
     }
 
+    /**
+     * @param {HTMLElement} popupRoot 
+    */
+    function registerPopupFormsHandlers( popupRoot ) {
+        if ( !( POPUP_OPTIONS && POPUP_OPTIONS.memorizeFormInputsValues) )
+            return;
+        
+        let forms = findForms( popupRoot );
+        if( !forms.length )
+            return;
+        
+        forms.forEach( form => {
+            formMemorizeInputValuesInLocalStorage( form );
+            addAttributesChangeObsever( popupRoot );
+            changeOnSubmitBehaviour( form, popupRoot );
+        });
+
+
+        function changeOnSubmitBehaviour(form, popupRoot) {
+            form.addEventListener('submit', evt => {
+                evt.preventDefault();
+                fetch(form.action, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ hello: 'AND', die: '111' })
+                }
+                ).then(response => { console.log( response) ;flushFormControlsAndLocalStorageValues(form); });
+            });
+        }
+
+        /**
+         * watch for class changes (popup closes and opens by adding/removing classes)
+         */
+        function addAttributesChangeObsever( popupRoot ) {
+            // create an observer instance
+            let observer = new MutationObserver(function (mutations) {
+                let mutationsArray = Array.from(mutations).filter(mutation => 
+                        mutation.type == 'attributes' &&
+                        mutation.attributeName == "class" &&
+                        !mutation.target.classList.contains('none') &&
+                        mutation.target.getBoundingClientRect().height == 0 );
+                
+                //form opening moment catched
+                if( !mutationsArray.length ) {
+                    forms.forEach( form => restoreControlsValuesFromLocalStorage(form) );
+                }
+
+            });
+
+            // configuration of the observer:
+            let config = {
+                attributes: true,
+                childList: false,
+                characterData: false
+            };
+
+            // pass in the target node, as well as the observer options
+            observer.observe(popupRoot, config);
+        }
+
+        /**
+         * @param {HTMLFormElement} form 
+         */
+        function formMemorizeInputValuesInLocalStorage( form ) {
+            // no fallback to cookies
+            if (!localStorage)
+                return;
+
+            let controls = searchInputControls(form);
+            if (!controls.length)
+                return;
+
+            //prepare Utils
+            if (!form.Utils)
+                initializeIdsGenerator(form);
+
+            controls.forEach(control => {
+                checkGenerateId(control);
+                control.addEventListener('change', evt => {
+                    localStorage.setItem(evt.target.id, evt.target.value);
+                });
+            });
+
+
+            /**
+            * @param {HTMLElement} control 
+            */
+            function checkGenerateId(control) {
+                if (!control.id)
+                    control.id = form.Utils.text.getRandomStrStrict(20);
+            }
+
+            /**
+             * initialize library and add it to root container
+             */
+            function initializeIdsGenerator(root) {
+                var _data = {};
+                _data.symbols = {};
+
+                var module = {};
+
+                module.text = {
+
+                    getRandomHex: function () {
+                        return (Math.floor(Math.random() * 0x100000) % 0x10000).toString(16);
+                    },
+
+                    getRandomHexFixed: function () {
+                        let _temp = this.getRandomHex();
+                        let _zeros = 4 - _temp.length;
+
+                        for (let i = 0; i < _zeros; i++)
+                            _temp = "0" + _temp;
+
+                        return _temp;
+                    },
+
+                    getUnicodeSymbol: function () {
+                        return String.fromCharCode(this.getRandomHexFixed());
+                    },
+
+                    /**
+                     * @param {string} symb  - Symbol to check
+                     * @param {RegExp} regex - Symbol should satisfy the conditions in regex
+                     */
+                    satisfyRestrictions: function (symb, regex = /[a-zA-Z0-9_]/) {
+                        return symb ? regex.test(symb) : false;
+                    },
+
+                    /**
+                     * @param {string} str        - String to check
+                     * @param {RegExpp[]} regexes - String should satisfy all the conditions listed in given Regexes array
+                     */
+                    satisfyRestrictionsStrict: function (
+                        str,
+                        regexes = [/[a-z]/, /[A-Z]/, /[0-9]/, /\_/]
+                    ) {
+                        return regexes.reduce((prev, curr, inx) => {
+                            return prev && curr.test(str);
+                        }, true);
+                    },
+
+                    getUnicodeSymbolNoRepeats: function () {
+                        let current;
+                        do { current = this.getUnicodeSymbol(); }
+                        while (current == _data.symbols.last);
+                        _data.symbols.last = current;
+                        return current;
+                    },
+
+                    getUnicodeSymbolNoRepeatsFiltered: function (filterRegex) {
+                        let current;
+                        do { current = this.getUnicodeSymbol(); }
+                        while (!this.satisfyRestrictions(current, filterRegex) || current == _data.symbols.last);
+                        _data.symbols.last = current;
+                        return current;
+                    },
+
+                    getRandomStr: function (len) {
+                        if (!len) return;
+                        let result = [];
+
+                        while (len--) {
+                            result.push(this.getUnicodeSymbolNoRepeatsFiltered());
+                        }
+
+                        return result.join('');
+                    },
+
+                    getRandomStrStrict: function (len) {
+                        if (!len) return;
+                        let result = "";
+
+                        do { result = this.getRandomStr(len); }
+                        while (!this.satisfyRestrictionsStrict(result));
+
+                        return result;
+                    },
+
+                    boldify: function (text) {
+                        return '<b>' + text + '</b>';
+                    }
+                };
+
+                if (!root.Utils)
+                    root.Utils = module;
+            }
+        }
+
+        /**
+         * @param {HTMLFormElement} form 
+         */
+        function restoreControlsValuesFromLocalStorage( form ) {
+            // no fallback to cookies
+            if (!localStorage)
+                return;
+
+            let controls = searchInputControls(form);
+            if (!controls.length)
+                return; 
+                
+            controls.forEach( control => { 
+                control.value = localStorage.getItem( control.id );
+            });
+        }
+
+
+        function flushFormControlsAndLocalStorageValues( form ) {
+            // no fallback to cookies
+            if (!localStorage)
+                return;
+
+            let controls = searchInputControls(form);
+            if (!controls.length)
+                return; 
+                
+            controls.forEach( control => { 
+                control.value = '';
+                localStorage.setItem( control.id, control.value );
+            });
+        }
+
+        /**
+         * @param {HTMLFormElement} form 
+         */
+        function searchInputControls(form) {
+            return Array.from( form.elements )
+                        .filter( element => !(element.attributes.type && element.attributes.type.nodeValue == 'submit') );
+        }
+
+        function getJsonDataFromInputControls( form ) {
+            let result;
+            
+            let controls = searchInputControls(form);
+            if (!controls.length)
+                return; 
+
+            controls.forEach( control => {
+                //TODO: get json from controls
+            });
+
+        }
+    }
+
 
     function generateOverlay() {
         //TODO: generate overlay HTML markup + css, add to <body>, return link to element
+    }
+
+    /**
+     * @param {HTMLElement} rootElement 
+     */
+    function findForms( rootElement ) {
+        return Array.from( rootElement.querySelectorAll('form') );
     }
 }
 
@@ -320,6 +560,10 @@ function sliderify( sliderContainer ) {
     
 }
 
+/**
+ * @description callback for google maps <script>
+ * @param {string} selector 
+ */
 function gMapsInitialize( selector='.popup-map-container' ) {
     
     findMapContainers( selector ).forEach( mapContainer => {
@@ -347,8 +591,4 @@ function gMapsInitialize( selector='.popup-map-container' ) {
         Array.from(gmapsFallbackImages).forEach( img => img.style.display = 'none');
     }
 }
-
-
-
-
 
